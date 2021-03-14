0168ffd06f139281c8bbeffdb1129091369b4821
BEren <bayram.eren@outlook.com.tr>
2021-03-15 00:34:23 +0300

    Commit 2

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
+  constructor(
+    private categoryService: CategoryService,
+    private productService: ProductService
+  ) {}
 
-  constructor() { }
+  categories: Category[] = [];
+  currentCategory: Category;
+  category: Category;
 
   ngOnInit(): void {
+    this.getCategory();
+  }
+  getCategory() {
+    this.categoryService.getCategory().subscribe((response) => {
+      this.categories = response.data;
+    });
+  }
+  setCurrentCategory(category: Category) {
+    this.currentCategory = category;
+  }
+  getCurrentCategoryClass(category: Category) {
+    //console.log(this.currentCategory.categoryId)
+    if (category == this.currentCategory) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
+    }
   }
 
+  getAllCategoryClass() {
+   // console.log(this.currentCategory)
+    if (!this.currentCategory) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
+    }
+  }
+  setAllCategory(category:Category={categoryId :0,categoryName:""}){
+    this.currentCategory = this.category;
+    this.getAllCategoryClass();
+  }
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,20 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-  products:any = [
-    {productId :1,productName:'Bardak',categoryId:1,unitPrice:5},
-    {productId :2,productName:'Kalem',categoryId:1,unitPrice:5},
-    {productId :3,productName:'Masa',categoryId:1,unitPrice:5},
-  ];
+  products: Product[] = [];
+  dataLoaded = false;
 
-  constructor() { }
+  constructor(private productService: ProductService,private activatedRoute:ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(
+      params => {
+        if (params["categoryId"]) {
+          this.getProductsByCategory(params["categoryId"])
+        } else {
+          this.getProducts()
+        }
+      }
+    )
+    this.getProducts();
+  }
+  getProducts() {
+    this.productService.getProducts().subscribe((response) => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    });
   }
 
+  getProductsByCategory(categoryId:number) {
+    this.productService.getProductCategory(categoryId).subscribe((response) => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    });
+  }
 }
